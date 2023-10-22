c47369bb4d88b1e727ed39efaf598380b3682ae1
chaosua <544218+chaosua@users.noreply.github.com>
2023-10-22 18:00:34 +0300

    some rework TODO:fix duplicates in search when language is not set to english

--- MainUnit.pas
+++ MainUnit.pas
@@ -8493,13 +8493,11 @@ end;
 procedure TMainForm.SearchItem;
 var
   i: integer;
-  name_was_set: bool;
   loc, ID, Name, QueryStr, WhereStr, t: string;
   class_, subclass, InventoryType, itemset, Quality_, flags, ItemLevel_: integer;
   Field: TField;
 begin
   loc:=LoadLocales();
-  name_was_set:=false;
   ShowHourGlassCursor;
   ID :=  edSearchItemEntry.Text;
   Name := edSearchItemName.Text;
@@ -8519,12 +8517,11 @@ begin
 
   if Name<>'%%' then
   begin
-    name_was_set:=true;
    if (loc<>'enUS') then begin
     if (WhereStr<> '') then
       WhereStr := Format('%s AND ((it.`name` LIKE ''%s'') OR (li.`name` LIKE ''%1:s'' AND li.`locale` = ''%s''))',[WhereStr, Name, loc])
     else
-      WhereStr := Format('WHERE (it.`name` LIKE ''%s'') OR (li.`name` LIKE ''%0:s'' AND li.`locale` = ''%s'')',[Name, loc]);
+      WhereStr := Format('WHERE ((it.`name` LIKE ''%s'') OR (li.`name` LIKE ''%0:s'' AND li.`locale` = ''%s''))',[Name, loc]);
    end
    else begin
     if (WhereStr<> '') then
@@ -8602,9 +8599,13 @@ begin
   if Trim(WhereStr)='' then
     if MessageDlg(dmMain.Text[134], mtConfirmation, mbYesNoCancel, -1)<>mrYes then Exit;
 
-  if (name_was_set=true) then begin
-      if (loc<>'enUS') then
+  if (loc<>'enUS') then begin
+      if (Name<>'%%') then begin
+        if ID<>'' then
+          QueryStr := Format('SELECT * FROM `item_template` it LEFT OUTER JOIN item_template_locale li ON it.entry=li.ID %s LIMIT 1',[WhereStr])
+        else
           QueryStr := Format('SELECT * FROM `item_template` it LEFT OUTER JOIN item_template_locale li ON it.entry=li.ID %s',[WhereStr])
+      end
       else
           QueryStr := Format('SELECT * FROM `item_template` it %s',[WhereStr])
   end
