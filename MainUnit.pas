ba368fe173d40b26533c3223238078d990f573f0
chaosua <544218+chaosua@users.noreply.github.com>
2024-01-29 23:06:43 +0200

    update cleaning flags

--- MainUnit.pas
+++ MainUnit.pas
@@ -3732,7 +3732,8 @@ begin
            ((Pos('ed'+s+'t',Components[i].Name)=1) or (Pos('ed'+s+'l',Components[i].Name)=1) or (Pos('ed'+s+'o',Components[i].Name)=1) or
             (Pos('me'+s+'t',Components[i].Name)=1) or (Pos('me'+s+'l',Components[i].Name)=1) or (Pos('me'+s+'o',Components[i].Name)=1) or
             (Pos('ed'+s+'riloc',Components[i].Name)=1) or (Pos('ed'+s+'orloc',Components[i].Name)=1) or
-            (Pos('ed'+s+'d',Components[i].Name)=1) or (Pos('ed'+s+'ri',Components[i].Name)=1) or (Pos('ed'+s+'ms',Components[i].Name)=1) ) then
+            (Pos('ed'+s+'d',Components[i].Name)=1) or (Pos('ed'+s+'ri',Components[i].Name)=1) or (Pos('ed'+s+'ms',Components[i].Name)=1) or
+            (Pos('ed'+s+'tloc',Components[i].Name)=1) {or} ) then
            TCustomEdit(Components[i]).Clear;
         if (Components[i] is TJvListView) and ((Pos('lv'+s+'o',Components[i].Name)=1) or (Pos('lv'+s+'l',Components[i].Name)=1) or (Pos('lv'+s+'t',Components[i].Name)=1)) then
           TCustomListView(Components[i]).Clear;
@@ -3744,7 +3745,7 @@ begin
            ((Pos('ed'+s+'v',Components[i].Name)=1) or (Pos('ed'+s+'p',Components[i].Name)=1) or (Pos('ed'+s+'a',Components[i].Name)=1)  or
            (Pos('ed'+s+'g',Components[i].Name)=1)  or (Pos('ed'+s+'x',Components[i].Name)=1)  or (Pos('ed'+s+'m',Components[i].Name)=1)  or
            (Pos('ed'+s+'s',Components[i].Name)=1) or (Pos('ed'+s+'r',Components[i].Name)=1) or (Pos('ed'+s+'i',Components[i].Name)=1) or
-           (Pos('ed'+s+'e',Components[i].Name)=1) or (Pos('ed'+s+'n',Components[i].Name)=1) or (Pos('ed'+s+'qi',Components[i].Name)=1) or (Pos('ed'+s+'tloc',Components[i].Name)=1) ) then
+           (Pos('ed'+s+'e',Components[i].Name)=1) or (Pos('ed'+s+'n',Components[i].Name)=1) or (Pos('ed'+s+'qi',Components[i].Name)=1) {or} ) then
              TCustomEdit(Components[i]).Clear;
         if (Components[i] is TJvListView) and ((Pos('lv'+s+'v',Components[i].Name)=1) or (Pos('lv'+s+'r',Components[i].Name)=1) or (Pos('lv'+s+'n',Components[i].Name)=1) or
            (Pos('lv'+s+'m',Components[i].Name)=1) or (Pos('lv'+s+'qi',Components[i].Name)=1)) then
@@ -3754,9 +3755,9 @@ begin
     begin
         if ((Components[i] is TLabeledEdit) or (Components[i] is TJvComboEdit) or (Components[i] is TMemo)) and
            ((Pos('ed'+s+'l',Components[i].Name)=1) or (Pos('ed'+s+'d',Components[i].Name)=1)
-             or (Pos('ed'+s+'p',Components[i].Name)=1)) or (Pos('ed'+s+'e',Components[i].Name)=1) then
+             or (Pos('ed'+s+'p',Components[i].Name)=1) or (Pos('ed'+s+'e',Components[i].Name)=1) {or} )then
              TCustomEdit(Components[i]).Clear;
-        if (Components[i] is TJvListView) and ((Pos('lv'+s+'o',Components[i].Name)=1) or (Pos('lv'+s+'l',Components[i].Name)=1) or (Pos('lv'+s+'t',Components[i].Name)=1) or (Pos('ed'+s+'tloc',Components[i].Name)=1) ) then
+        if (Components[i] is TJvListView) and ((Pos('lv'+s+'o',Components[i].Name)=1) or (Pos('lv'+s+'l',Components[i].Name)=1) or (Pos('lv'+s+'t',Components[i].Name)=1) {or} ) then
           TCustomListView(Components[i]).Clear;
     end;
     if s='h' then
@@ -3767,12 +3768,6 @@ begin
              TCustomEdit(Components[i]).Clear;
         if (Components[i] is TJvListView) and ((Pos('lv'+s+'o',Components[i].Name)=1) or (Pos('lv'+s+'l',Components[i].Name)=1) or (Pos('lv'+s+'t',Components[i].Name)=1)) then
           TCustomListView(Components[i]).Clear;
-    end;
-    if s='g' then
-    begin
-        if ((Components[i] is TLabeledEdit) or (Components[i] is TJvComboEdit) or (Components[i] is TMemo)) and
-           ((Pos('ed'+s+'tloc',Components[i].Name)=1) {or}) then
-             TCustomEdit(Components[i]).Clear;
     end;
   end;
 end;
